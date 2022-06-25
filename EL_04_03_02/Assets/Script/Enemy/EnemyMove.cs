053d55a71443f2ed11016c944e353260e4e37f4c
Programmer-7501 <gameprogrammerforest10@gmail.com>
2022-06-25 13:25:30 +0900

    Enemyの追跡処理追加

--- EL_04_03_02/Assets/Script/Enemy/EnemyMove.cs
+++ EL_04_03_02/Assets/Script/Enemy/EnemyMove.cs
@@ -1,21 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class EnemyMove : MonoBehaviour
 {
-    int Count = 0;
-
-    // Start is called before the first frame update
+    public GameObject TargetObject; /// 目標位置
+    NavMeshAgent m_navMeshAgent; /// NavMeshAgent
+    // Use this for initialization
     void Start()
     {
-        
+        // NavMeshAgentコンポーネントを取得
+        m_navMeshAgent = GetComponent<NavMeshAgent>();
     }
-
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.forward;
+        // NavMeshが準備できているなら
+        if (m_navMeshAgent.pathStatus != NavMeshPathStatus.PathInvalid)
+        {
+            // NavMeshAgentに目的地をセット
+            m_navMeshAgent.SetDestination(TargetObject.transform.position);
+        }
+    }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("Result");
+        }
     }
 }
