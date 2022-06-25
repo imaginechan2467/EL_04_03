57bbbb3db321f52002040e8ed1246bc0b5bce8a7
KIKUCHIKAIRI\sao_k <sao_kazuto@icloud.com>
2022-06-25 11:57:12 +0900

    add-scoremanager

--- EL_04_03_02/Assets/Script/ScoreManager.cs
+++ EL_04_03_02/Assets/Script/ScoreManager.cs
@@ -5,22 +5,31 @@ using UnityEngine;
 
 public static class ScoreManager
 {
-    static int score = 0;
+    enum ScoreName
+    {
+        SN_Time,
+        SN_Score,
+        SN_MAX
+    }
 
+    static int[] score = new int[(int)ScoreName.SN_MAX];
     // リセット
     public static void Reset()
     {
-        score = 0;
+        for (int i = 0; i < score.Length; i++)
+        {
+            score[i] = 0;
+        }
     }
     // スコア追加か減点
-    public static void Add(int added)
+    public static void Add(int added, int name)
     {
-        score += added;
-        if (score < 0) { score = 0; }
+        score[name] += added;
+        if (score[name] < 0) { score[name] = 0; }
     }
     // スコア取得
-    public static int Get()
+    public static int Get(int name)
     {
-        return score;
+        return score[name];
     }
 }
