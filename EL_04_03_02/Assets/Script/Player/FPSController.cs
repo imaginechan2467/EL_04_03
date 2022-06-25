e40c6ff4ad93fcd8cbe8663d4a5590a66355b249
KIKUCHIKAIRI\sao_k <sao_kazuto@icloud.com>
2022-06-25 14:41:09 +0900

    add

--- EL_04_03_02/Assets/Script/Player/FPSController.cs
+++ EL_04_03_02/Assets/Script/Player/FPSController.cs
@@ -122,8 +122,10 @@ public class FPSController : MonoBehaviour
         }
         if (other.gameObject.CompareTag("Gate"))
         {
-            ScoreManager.Add(2,(int)ScoreManager.ScoreName.SN_ClearType);
+            if (ScoreManager.Get((int)ScoreManager.ScoreName.SN_ClearType) == 1) { 
+                ScoreManager.Add(2, (int)ScoreManager.ScoreName.SN_ClearType);
             SceneManager.LoadScene("Result");
+        }
         }
     }
     private void OnTriggerExit(Collider other)
